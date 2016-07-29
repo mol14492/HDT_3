dca6dcfa37e0eb3f232be1b14cb3f7693de367f9
Jose Molina <mol14492@uvg.edu.gt>
2016-07-29 16:10:16 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+
 public class Main {
 
-  public static void main(String []args) {
-    
-      }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int contador=3000;
+		int intR;
+
+		while(contador > 0){
+			Random numeroR = new Random();
+			intR = numeroR.getRandom();
+			Random escribir = new Random();
+			escribir.crearTxt(intR);
+			contador--;
+	}
+
+}
 }
