3ce367c9a07123ddd0c862407917383f4b1498d0
Jose Molina <mol14492@uvg.edu.gt>
2016-07-30 13:10:07 -0600

    // Se le agrego el sort

--- Main.java
+++ Main.java
@@ -1,18 +1,34 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int contador=3000;
+
 		int intR;
+		Scanner scan = new Scanner( System.in );
+        
+        
+			GetRandom escribir = new GetRandom();
+			escribir.crearTxt();
+			GetRandom array = new GetRandom();
+			String [] arr=array.guardarArray();
+				
+			int numeros[]={0,1,2};
+					;
+			// Sort
+			RadixSort comp= new RadixSort();
 
-		while(contador > 0){
-			Random numeroR = new Random();
-			intR = numeroR.getRandom();
-			Random escribir = new Random();
-			escribir.crearTxt(intR);
-			contador--;
+		     
+			
+		        for (i = 0; i < n; i++)
+		            arre[i] = scan.nextInt();
+		        /** Call method sort **/
+		        comp.radixSort(arre);
+		        /** Print sorted Array **/
+		        System.out.println("\nElements after sorting ");
+		        for (i = 0; i < n; i++)
+		            System.out.print(arre[i]+" ");
+		        System.out.println();
 	}
-
-}
 }
