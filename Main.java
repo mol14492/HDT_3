cea16e2791303b5d649689c376a476a25cdf3727
bau192 <bau15192@uvg.edu.gt>
2016-07-31 12:33:57 -0600

    array llamado intArray servira para el programa
    el intArray ya es un array de int y no de String

--- Main.java
+++ Main.java
@@ -1,32 +1,50 @@
 import java.util.Scanner;
+import java.io.*;
 
-public class Main {
+public class main {
 
-	public static void main(String[] args) {
+	//tamaño de array(aunque no lo pude implementar como variable y tire el valor directo)
+	int n=3000;
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
 		int intR;
 		Scanner scan = new Scanner( System.in );
         
-        
+		
 			GetRandom escribir = new GetRandom();
 			escribir.crearTxt();// se crea archivo con 3000 numeros random.
 			GetRandom array = new GetRandom(); 
 			String [] arr=array.guardarArray();//se crea el array con los numeros del archivo 
-				
-			int numeros[]={0,1,2};//aarray de ints solo para que no de error, esto no es parte del programa es oslo para probar.
-					;
-			// Sort, se llama al radixsort y al array para compararlos y ordenarlos
-			RadixSort comp= new RadixSort();
+			try
+				{
 			
-		        for (i = 0; i < n; i++)
-		            arre[i] = scan.nextInt();// se recorre un array de tipo int, nosostros tenemos uno de tipo string
-		        /** Call method sort **/
-		        comp.radixSort(arre);// se llama un array de tipo int, nosostros tenemos uno de tipo string
-		        /** Print sorted Array **/
-		        System.out.println("\nElements after sorting ");
-		        for (i = 0; i < n; i++)
-		            System.out.print(arre[i]+" ");// imprime array de tipo int, nosostros tenemos uno de tipo string
-		        System.out.println();
+				FileInputStream archStream  = new FileInputStream("file.txt");
+				// Creamos el objeto de entrada
+				DataInputStream entrada = new DataInputStream(archStream);
+				// Creamos el Buffer de Lectura
+				BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
+				//se crea el array de tamaño de 3000 elementos (0-2999)
+				int [] intArray=new int [3000];
+				
+				//ciclo para llenar el array de ints / USAREMOS EL ARRAY llamado intArray
+				for (int i=0;i<3000;i++)
+					{
+					String strNum=buffer.readLine();
+					int intNum = Integer.parseInt(strNum);
+					intArray[i]=intNum;
+					//System.out.print(intArray[i]);
+					//System.out.println("----- >"+ i);
+					}
+				}
+			//catch par errores durante el proceso
+			catch(Exception e)
+				{
+			
+				System.err.println("error durante escritura/creacion de .txt/array" + e.getMessage());
+			
+				}
+				
 	}
 }
