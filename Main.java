28298e5d726629cfee774aae880a90a8da9456cc
Jose Molina <mol14492@uvg.edu.gt>
2016-07-30 22:11:17 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,25 +10,23 @@ public class Main {
         
         
 			GetRandom escribir = new GetRandom();
-			escribir.crearTxt();
-			GetRandom array = new GetRandom();
-			String [] arr=array.guardarArray();
+			escribir.crearTxt();// se crea archivo con 3000 numeros random.
+			GetRandom array = new GetRandom(); 
+			String [] arr=array.guardarArray();//se crea el array con los numeros del archivo 
 				
-			int numeros[]={0,1,2};
+			int numeros[]={0,1,2};//aarray de ints solo para que no de error, esto no es parte del programa es oslo para probar.
 					;
-			// Sort
+			// Sort, se llama al radixsort y al array para compararlos y ordenarlos
 			RadixSort comp= new RadixSort();
-
-		     
 			
 		        for (i = 0; i < n; i++)
-		            arre[i] = scan.nextInt();
+		            arre[i] = scan.nextInt();// se recorre un array de tipo int, nosostros tenemos uno de tipo string
 		        /** Call method sort **/
-		        comp.radixSort(arre);
+		        comp.radixSort(arre);// se llama un array de tipo int, nosostros tenemos uno de tipo string
 		        /** Print sorted Array **/
 		        System.out.println("\nElements after sorting ");
 		        for (i = 0; i < n; i++)
-		            System.out.print(arre[i]+" ");
+		            System.out.print(arre[i]+" ");// imprime array de tipo int, nosostros tenemos uno de tipo string
 		        System.out.println();
 	}
 }
