84916e5149b5ccab6d7554f22f19ff38c0be07da
Jose Molina <mol14492@uvg.edu.gt>
2016-08-01 11:47:53 -0600

    Update GetRandom.java

--- GetRandom.java
+++ GetRandom.java
@@ -3,15 +3,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 
 /**
- * 
+ * guardarArray
  */
 
 /**
- * @author Gera
+ * @author Gera/Bautista
  *
  */
 public class GetRandom implements I_random 
@@ -41,10 +42,12 @@ public class GetRandom implements I_random
 		  File out = new File("file.txt");//Se crea archivo de texto.
 	        FileWriter fw = null;
 	        int cont = 3000;//contador 3000
-	        try {
+	        try 
+			{
 	            fw = new FileWriter(out);
 	            BufferedWriter writer = new BufferedWriter(fw);// Buffer para escribir en el archivo
-	            while (cont > 0) {
+	            while (cont > 0) 
+				{
 					int intR = numeroR.getRandom();//genera numero random
 					String linea = Integer.toString(intR);//numero random a String
 	                writer.write(linea);//escribe el numero en el archivo
@@ -52,23 +55,30 @@ public class GetRandom implements I_random
 	                cont--;//resta uno al contador
 	            }
 	            writer.close();//se cierra el archivo.
-	        } catch (IOException e) {
+	        } 
+			catch (IOException e) 
+			{
 	            e.printStackTrace();
 	            System.exit(0);
 	        }
 	  }
-	public String[] guardarArray(){
+	public String[] guardarArray()
+	{
 
 		    String token1 = "";
 		    Scanner file1=null;
-			try {
+			try 
+			{
 			file1 = new Scanner(new File("file.txt")).useDelimiter(",\\s*");// se abre el archivo
-			} catch (FileNotFoundException e) {
+			} 
+			catch (FileNotFoundException e) 
+			{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		    List<String> temps = new ArrayList<String>();// se crea un arraylist
-		    while (file1.hasNext()) {//mientras haya algoq que leer.
+		    while (file1.hasNext()) //mientras haya algoq que leer.
+			{
 
 		      token1 = file1.next(); //el token apunta al primer numero
 		      temps.add(token1);// se agrega la lista el numero 
@@ -77,9 +87,29 @@ public class GetRandom implements I_random
 
 		    String[] tempsArray = temps.toArray(new String[0]); //se crea un array de la lista temps
 
-			return tempsArray;// regresa el array de strings con los numeros.
-		    
-		  }
 		
+			return tempsArray;// regresa el array de strings con los numeros. 
+		  }
+		  
+		   public void crearTxtn(int[] a, String nombre)
+	  {	
+		 try
+		 {
+		     PrintWriter pr = new PrintWriter(nombre);    
+
+		     for (int i=0; i<3000 ; i++)
+		     {
+		         pr.println(a[i]);
+		     }
+		     pr.close();
+		 }
+		 catch (Exception e)
+		 {
+		     e.printStackTrace();
+		     System.out.println("No existe el archivo.");
+		 }
+		 
 	  }
+	  }
+
 
